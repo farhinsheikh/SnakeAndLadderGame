4c0d90513d48cdfa2ba969c7bbe238ad3eb532ea
farhinsheikh <farhinsheikh2015@gmail.com>
2021-04-16 02:03:35 +0530

    [Farhin]ADDED:Random function for die roll

--- Program.cs
+++ Program.cs
@@ -4,12 +4,19 @@ namespace SnakeAndLadderGame
 {
     class Program
     {
+        const int StartPosition = 0;
+        public void getDieRoll()
+        {
+            Random random = new Random();
+            int die = random.Next(1, 7);
+            Console.WriteLine(" Player's start position is = " + StartPosition);
+            Console.WriteLine(" Your number is = " + die);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Snake and Ladder Game");
-            const int START_POSITION = 0;
-            int player = 1;
-            Console.WriteLine(" Player position = " + START_POSITION);
+            Program play = new Program();
+            play.getDieRoll();
         }
     }
 }
+
