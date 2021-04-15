cafbf8d61f6d1cef936363202425c69d2bc3888c
farhinsheikh <farhinsheikh2015@gmail.com>
2021-04-16 01:33:04 +0530

    [Farhin]ADDED:Variable for player and position

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace SnakeAndLadderGame
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake and Ladder Game");
+            const int START_POSITION = 0;
+            int player = 1;
+            Console.WriteLine(" Player position = " + START_POSITION);
         }
     }
 }
